fe35653ed6c2d2d0503c21c4edee71d68220c896
Francesco Francomano <francesco.francomano@relatech.com>
2019-03-22 16:19:29 +0100

    First import

---
+++ src/main/java/com/relhardware/auth/RelhardwareSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.relhardware.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelhardwareSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelhardwareSecurityApplication.class, args);
+	}
+
+}
